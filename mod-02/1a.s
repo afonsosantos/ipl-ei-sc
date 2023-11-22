f0aad9535c518b0e15eff75cb7021d9bba6beb63
Afonso Santos <afonso.faisca1@gmail.com>
2023-11-22 21:19:54 +0000

    Base file

--- mod-02/1a.s
+++ mod-02/1a.s
@@ -1,18 +1,18 @@
-		.data
-i:		.word	0
-C:		.word	7
-A:		.space	800
-B:		.space	800
+.data
+	i:		.word	0
+	C:		.word	7
+	A:		.space	800
+	B:		.space	800
 
-		.text
+.text
 
 	DADDI	R1,R0,0	;inicializa i 
 	SD	R1, i(R0)	;armazena i 
 	
 loop:
 
-	LD	R1,i(R0)	;obtÈm o valor de i 
-	DADDI	R9,R0,8		;tamanho da dword para multiplicaÁ„o pelo Ìndice
+	LD	R1,i(R0)	;obt√©m o valor de i 
+	DADDI	R9,R0,8		;tamanho da dword para multiplica√ß√£o pelo √≠ndice
 	DMUL	R2,R1,R9	;R2 = offset de i
 
 	LD	R4,B(R2)	;carrega o valor de B[i] 
@@ -20,17 +20,17 @@ loop:
 
 	DADD	R6,R4,R5	;B[i]+C 
 
-	LD	R1,i(R0)	;obtÈm o valor de i 
-	DADDI	R9,R0,8		;tamanho da dword para multiplicaÁ„o pelo Ìndice
+	LD	R1,i(R0)	;obt√©m o valor de i 
+	DADDI	R9,R0,8		;tamanho da dword para multiplica√ß√£o pelo √≠ndice
 	DMUL	R2,R1,R9	;R2 = offset de i
 	SD	r6,A(r2)	;A[i] = B[i]+C 
 
-	LD	R1,i(R0)	;obtÈm o valor de i 
+	LD	R1,i(R0)	;obt√©m o valor de i 
 	DADDI	R1,R1,1		;incrementa i 
 	SD	r1,i(R0)	;armazena i 
 
-	LD	R1,i(R0)		;obtÈm o valor de i 
+	LD	R1,i(R0)		;obt√©m o valor de i 
 	DADDI	R8,R1,-100	;o contador chegou a 100? 
-	BNEZ	R8,loop		;se n„o chegou, repete o ciclo 
+	BNEZ	R8,loop		;se n√©o chegou, repete o ciclo 
 
 	halt			;termina a execucao 
