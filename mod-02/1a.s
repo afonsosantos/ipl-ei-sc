3ddbd36244fdf61ba646b16efedab65427901140
Afonso Santos <afonso.faisca1@gmail.com>
2023-11-22 21:21:38 +0000

    Formatting

--- mod-02/1a.s
+++ mod-02/1a.s
@@ -1,4 +1,5 @@
 .data
+
 	i:		.word	0
 	C:		.word	7
 	A:		.space	800
@@ -6,31 +7,31 @@
 
 .text
 
-	DADDI	R1,R0,0	;inicializa i 
-	SD	R1, i(R0)	;armazena i 
+	daddi	R1,R0,0			; inicializa i 
+	sd	R1, i(R0)			; armazena i 
 	
 loop:
 
-	LD	R1,i(R0)	;obtém o valor de i 
-	DADDI	R9,R0,8		;tamanho da dword para multiplicação pelo índice
-	DMUL	R2,R1,R9	;R2 = offset de i
+	ld	R1,i(R0)			; obtém o valor de i 
+	daddi	R9,R0,8			; tamanho da dword para multiplicação pelo índice
+	dmul	R2,R1,R9		; R2 = offset de i
 
-	LD	R4,B(R2)	;carrega o valor de B[i] 
-	LD	R5,C(R0)	;carrega o valor de C 
+	ld	R4,B(R2)			; carrega o valor de B[i] 
+	ld	R5,C(R0)			; carrega o valor de C 
 
-	DADD	R6,R4,R5	;B[i]+C 
+	dadd	R6,R4,R5		; B[i]+C 
 
-	LD	R1,i(R0)	;obtém o valor de i 
-	DADDI	R9,R0,8		;tamanho da dword para multiplicação pelo índice
-	DMUL	R2,R1,R9	;R2 = offset de i
-	SD	r6,A(r2)	;A[i] = B[i]+C 
+	ld	R1,i(R0)			; obtém o valor de i 
+	daddi	R9,R0,8			; tamanho da dword para multiplicação pelo índice
+	dmul	R2,R1,R9		; R2 = offset de i
+	sd	r6,A(r2)			; A[i] = B[i]+C 
 
-	LD	R1,i(R0)	;obtém o valor de i 
-	DADDI	R1,R1,1		;incrementa i 
-	SD	r1,i(R0)	;armazena i 
+	ld	R1,i(R0)			; obtém o valor de i 
+	daddi	R1,R1,1			; incrementa i 
+	sd	r1,i(R0)			; armazena i 
 
-	LD	R1,i(R0)		;obtém o valor de i 
-	DADDI	R8,R1,-100	;o contador chegou a 100? 
-	BNEZ	R8,loop		;se néo chegou, repete o ciclo 
+	ld	R1,i(R0)			; obtém o valor de i 
+	daddi	R8,R1,-100		; o contador chegou a 100? 
+	bnez	R8,loop			; se néo chegou, repete o ciclo 
 
-	halt			;termina a execucao 
+	halt					; termina a execucao 
