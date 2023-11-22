a948e53709020c7d026e2b9dfd8ce666ba0dea60
Afonso Santos <afonso.faisca1@gmail.com>
2023-11-22 23:11:53 +0000

    Exercise d

--- mod-02/1d.s
+++ mod-02/1d.s
@@ -7,22 +7,23 @@
 
 .text
 
-	daddi	R1,R0,8			; inicializa i a 8
+	daddi	R1,R0,0			; inicializa i a 8
 	ld	R5,C(R0)			; carrega o valor de C
 	
 loop:
 
-	dsll r2, r1, 3
 	ld	R4,B(R2)			; carrega o valor de B[i]
 	
 	; 1 bolha (r4)
 	dadd	R6,R4,R5		; B[i] + C 
-	sd	r6,A(r2)			; A[i] = B[i] + C 
+	sd	r6,A(r1)			; A[i] = B[i] + i 
 	daddi	R1,R1,8			; incrementa i em 8
 	daddi	R8,R1,-800		; o contador chegou a 800? 
 
 	; 1 bolha (r8)
 	bnez	R8,loop			; se não chegou, repete o ciclo 
+
+	dsra r1,r1,8			; divide valor por 8 para obter 100
 	
 	; 1 bolha de conflito de controlo
 	sd	r1,i(R0)			; armazena i 
